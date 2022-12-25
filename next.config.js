53ff915418817ec741372b2c04727e0b3a066a52
Pavel Vostrikov <mail@vostrik.com>
2022-12-25 23:28:49 +0000

    feat: split i18n and next configs

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin')
 const withVanillaExtract = createVanillaExtractPlugin()
 
+const { i18n } = require('./next-i18next.config')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en']
-  },
+  i18n,
   reactStrictMode: true
 }
-
-
 
 module.exports = withVanillaExtract(nextConfig)
